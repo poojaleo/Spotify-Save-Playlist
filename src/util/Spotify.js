c213f8237080d53abe7b5731e21731a2a204281a
Pooja Patel <poojapatel.leo@gmail.com>
2022-03-22 14:10:24 -0700

    Removed unused variables

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,62 +1,10 @@
 let accessToken;
-let refreshToken;
 
 const TOKEN = "https://accounts.spotify.com/api/token";
 
 
 const clientId = "260ae191a73347388f0906143e73f889";
-const redirectURI = "https://spotifyplaylistsave.netlify.app/";
 const clientSecret = "01204551aceb4658a34da9212418d490";
-
-function getCode() {
-    let code = null;
-    const queryString = window.location.search;
-    if ( queryString.length > 0 ){
-        const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get('code')
-    }
-    return code;
-}
-
-function handleAuthorizationResponse(){
-    if ( this.status === 200 ){
-        const data = JSON.parse(this.responseText);
-        console.log(data);
-        if ( data.access_token !== undefined ){
-            accessToken = data.access_token;
-            localStorage.setItem("accessToken", accessToken);
-        }
-        if ( data.refresh_token  !== undefined ){
-            refreshToken = data.refresh_token;
-            localStorage.setItem("refreshToken", refreshToken);
-        }
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-
-    return accessToken;
-}
-
-function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", TOKEN, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(clientId + ":" + clientSecret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-}
-
-function fetchAccessToken( code ){
-
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code;
-    body += "&redirect_uri=" + encodeURI(redirectURI);
-    body += "&client_id=" + clientId;
-    body += "&client_secret=" + clientSecret;
-    callAuthorizationApi(body);
-}
 
 const Spotify = {
     async getAccessToken() {
@@ -65,7 +13,7 @@ const Spotify = {
             return accessToken;
         }
 
-        const result = await fetch('https://accounts.spotify.com/api/token', {
+        const result = await fetch(TOKEN, {
             method: 'POST',
             headers: {
                 'Content-Type' : 'application/x-www-form-urlencoded',
@@ -81,6 +29,7 @@ const Spotify = {
 
     async search(term) {
         const accessToken = await Spotify.getAccessToken();
+
         const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
